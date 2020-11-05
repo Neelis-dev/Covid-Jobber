280fc841dc9511262df520066c26de3f190baed6
Neelis Rüter <neelissimo@gmail.com>
2020-11-05 15:38:17 +0100

    found API that works

--- app/src/main/java/com/example/covid_jobber/activities/MainActivity.java
+++ app/src/main/java/com/example/covid_jobber/activities/MainActivity.java
@@ -2,21 +2,37 @@ package com.example.covid_jobber.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentProviderOperation;
 import android.os.Bundle;
 
 import com.example.covid_jobber.R;
+import com.example.covid_jobber.databinding.ActivityMainBinding;
 import com.example.covid_jobber.fragments.CardDisplayFragment;
 
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
 public class MainActivity extends AppCompatActivity {
+    public ActivityMainBinding mainBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
 
         if(findViewById(R.id.card_display_fragment) != null){
             getSupportFragmentManager().beginTransaction().
                     replace(R.id.card_display_fragment,new CardDisplayFragment()).commitAllowingStateLoss();
         }
+
     }
 
 
--- app/src/main/java/com/example/covid_jobber/fragments/CardDisplayFragment.java
+++ app/src/main/java/com/example/covid_jobber/fragments/CardDisplayFragment.java
@@ -9,6 +9,22 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.covid_jobber.R;
+import com.example.covid_jobber.databinding.FragmentCardDisplayBinding;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,9 +33,10 @@ import com.example.covid_jobber.R;
  */
 public class CardDisplayFragment extends Fragment {
 
-
+    public FragmentCardDisplayBinding fragmentCardDisplayBinding;
     public CardDisplayFragment() {
         // Required empty public constructor
+
     }
 
 
@@ -31,6 +48,29 @@ public class CardDisplayFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        fragmentCardDisplayBinding = FragmentCardDisplayBinding.inflate(getLayoutInflater());
+
+        final String appId = "64fa1822";
+        final String appKey = "d41a9537116b72a1c2a890a27376d552";
+
+        Request request = new Request.Builder()
+                .url("https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id="+appId+"&app_key="+appKey)
+                .build();
+
+        OkHttpClient client = new OkHttpClient();
+        client.newCall(request).enqueue(new Callback() {
+            @Override public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override public void onResponse(Call call, Response response) throws IOException {
+                ResponseBody responseBody = response.body();
+                JSONObject jsonObject=null;
+                System.out.println("it worked");
+                System.out.println(responseBody.string());
+                fragmentCardDisplayBinding.tvJobDescription.setText("If you can see this the API responded successfully! no Error code!");
+            }
+        });
     }
 
     @Override
@@ -39,4 +79,5 @@ public class CardDisplayFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_card_display, container, false);
     }
+
 }
