ba5e08afc9e57ed27c9610cd1a87687e8a4429a2
Insa <insa.belter@gmx.de>
2020-11-13 14:14:39 +0100

    Fixed swipe fragment binding

--- app/src/main/java/com/example/covid_jobber/fragments/SwipeFragment.java
+++ app/src/main/java/com/example/covid_jobber/fragments/SwipeFragment.java
@@ -1,5 +1,6 @@
 package com.example.covid_jobber.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -12,6 +13,8 @@ import com.example.covid_jobber.R;
 import com.example.covid_jobber.classes.ApiHandler;
 import com.example.covid_jobber.databinding.FragmentSwipeBinding;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link SwipeFragment#newInstance} factory method to
@@ -21,32 +24,39 @@ public class SwipeFragment extends Fragment {
 
     private ApiHandler apiHandler = new ApiHandler();
 
-    public FragmentSwipeBinding fragmentSwipeBinding;
+    private FragmentSwipeBinding binding;
 
     public SwipeFragment() {
         // Required empty public constructor
     }
 
-
-    // TODO: Rename and change types and number of parameters
     public static SwipeFragment newInstance() {
         SwipeFragment fragment = new SwipeFragment();
         return fragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        apiHandler.forUI();
-
-        this.fragmentSwipeBinding = FragmentSwipeBinding.inflate(getLayoutInflater());
-        this.fragmentSwipeBinding.tvJobDescription.setText("Blabla");
+    public View onCreateView(@NotNull LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentSwipeBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        return view;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_swipe, container, false);
+    public void onStart() {
+        super.onStart();
+
+        apiHandler.forUI();
+
+        String exampleText = "example";
+        binding.tvJobName.setText(exampleText);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
